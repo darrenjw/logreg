9972c76e11a91bbdea050b98f716cb327d1f020d
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2022-05-08 21:33:56 +0100

    MH fit in python with numpy

--- Python/fit-numpy.py
+++ Python/fit-numpy.py
@@ -5,11 +5,13 @@
 import os
 import pandas as pd
 import numpy as np
+import scipy as sp
+import scipy.stats
 from scipy.optimize import minimize
 
 df = pd.read_parquet(os.path.join("..", "pima.parquet"))
 print(df)
-n,p = df.shape
+n, p = df.shape
 print(n, p)
 
 y = pd.get_dummies(df["type"])["Yes"].to_numpy(dtype='float32')
@@ -30,7 +32,82 @@ res = minimize(lambda x: -ll(x), init, method='BFGS')
 print(res.x)
 print(ll(res.x))
 
+print("MAP next:")
 
+def lprior(beta):
+    return (sp.stats.norm.logpdf(beta[0], loc=0, scale=10) + 
+            np.sum(sp.stats.norm.logpdf(beta[range(1,p)], loc=0, scale=1)))
+
+print(lprior(init))
+
+def lpost(beta):
+    return ll(beta) + lprior(beta)
+
+print(lpost(init))
+res = minimize(lambda x: -lpost(x), init, method='BFGS')
+print(res.x)
+print(ll(res.x))
+
+print("Next, MH:")
+
+def metHet(init, lpost, rprop, dprop = lambda new, old: 1.,
+           thin = 10, iters = 10000, verb = True):
+    p = len(init)
+    olp = -np.inf
+    mat = np.zeros((iters, p))
+    x = init
+    if (verb):
+        print(str(iters) + " iterations")
+    for i in range(iters):
+        if (verb):
+            print(str(i), end=" ", flush=True)
+        for j in range(thin):
+            prop = rprop(x)
+            lp = lpost(prop)
+            a = lp - olp + dprop(x, prop) - dprop(prop, x)
+            if (np.log(np.random.rand()) < a):
+                x = prop
+                olp = lp
+        mat[i,:] = x
+    if (verb):
+        print("\nDone.")
+    return mat
+
+pre = np.concatenate((np.array([10]), np.ones(p-1)))
+
+def rprop(beta):
+    return beta + 0.02*pre*np.random.randn(p)
+
+out = metHet(res.x, lpost, rprop, thin=10)
+
+print(out)
+print("Posterior summaries:")
+summ = scipy.stats.describe(out)
+print(summ)
+print("\nMean: " + str(summ.mean))
+print("Variance: " + str(summ.variance))
+
+import matplotlib.pyplot as plt
+figure, axis = plt.subplots(4, 2)
+for i in range(8):
+    axis[i // 2, i % 2].plot(range(out.shape[0]), out[:,i])
+    axis[i // 2, i % 2].set_title(f'Trace plot for the variable {i}')
+plt.savefig("np-trace.png")
+#plt.show()
+
+figure, axis = plt.subplots(4, 2)
+for i in range(8):
+    axis[i // 2, i % 2].hist(out[:,i], 50)
+    axis[i // 2, i % 2].set_title(f'Histogram for variable {i}')
+plt.savefig("np-hist.png")
+#plt.show()
+
+figure, axis = plt.subplots(4, 2)
+for i in range(8):
+    axis[i // 2, i % 2].acorr(out[:,i], maxlags=100)
+    axis[i // 2, i % 2].set_title(f'ACF for variable {i}')
+plt.savefig("np-acf.png")
+#plt.show()
 
 
 
