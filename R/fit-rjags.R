8aa12edc74a624023211fe14ea954b631b548915
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2022-05-10 13:43:16 +0100

    rjags model working correctly

--- R/fit-rjags.R
+++ R/fit-rjags.R
@@ -15,7 +15,7 @@ X = cbind(Int=1, X)
 print(y[1:6])
 print(head(X))
 
-set.seed(43)
+set.seed(43) # fix seed to avoid choosing inits jags doesn't like
 init = rnorm(p, 0.1)
 names(init) = colnames(X)
 data = list(X=X, y=as.integer(y), p=p, n=n)
@@ -25,19 +25,24 @@ modelstring="
       y[i] ~ dbern(pr[i])
       logit(pr[i]) <- inprod(X[i,], beta)
     }
-    beta[1] ~ dnorm(0, 0.1)
+    beta[1] ~ dnorm(0, 0.01)
     for (j in 2:p) {
       beta[j] ~ dnorm(0, 1)
     }
   }
 "
-model=jags.model(textConnection(modelstring), data=data, inits=list(beta=init))
-update(model,n.iter=100)
-output=coda.samples(model=model,variable.names=c("beta"), n.iter=10000, thin=1)
-print(summary(output))
-plot(output)
-
-
+model = jags.model(textConnection(modelstring),
+                   data=data, inits=list(beta=init))
+update(model, n.iter=1000) # burn in
+thin = 10 # set thinning level here
+output = coda.samples(model=model, variable.names=c("beta"),
+                      n.iter=10000*thin, thin=thin)
+#print(summary(output))
+#plot(output)
+out = as.matrix(output)
+mcmcSummary(out)
+image(cor(out)[ncol(out):1,])
+pairs(out[sample(1:10000,1000),],pch=19,cex=0.2)
 
 
 
