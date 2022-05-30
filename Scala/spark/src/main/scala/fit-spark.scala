f6b4e18c3ad8a6a2780e7e30de222fbb343082d6
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2022-05-30 16:21:55 +0100

    factor out the MH kernel

--- Scala/spark/src/main/scala/fit-spark.scala
+++ Scala/spark/src/main/scala/fit-spark.scala
@@ -6,18 +6,35 @@ import org.apache.spark.ml.linalg.{DenseVector, Vectors}
 import org.apache.spark.sql.functions.udf
 
 import smfsb._
-import breeze.stats.distributions.Gaussian
+import breeze.stats.distributions.{Gaussian, Uniform}
 import breeze.linalg.{DenseVector => BDV, _}
 import breeze.numerics._
 
 object FitSpark {
 
-  def toBDV(v: DenseVector): BDV[Double] = v match {
+  type DVD = BDV[Double]
+
+  def mhKernel(logPost: DVD => Double, rprop: DVD => DVD,
+    dprop: (DVD, DVD) => Double = (n, o) => 1.0): ((DVD, Double)) => (DVD, Double) = {
+      val r = Uniform(0.0,1.0)
+      state => {
+        val (x0, ll0) = state
+        val x = rprop(x0)
+        val ll = logPost(x)
+        val a = ll - ll0 + dprop(x0, x) - dprop(x, x0)
+        if (math.log(r.draw()) < a)
+          (x, ll)
+        else
+          (x0, ll0)
+      }
+  }
+
+  def toBDV(v: DenseVector): DVD = v match {
     case DenseVector(vals) => BDV[Double](vals)
   }
 
-  def toDV(v: BDV[Double]): DenseVector = v match {
-    case v: BDV[Double] => new DenseVector(v.toArray)
+  def toDV(v: DVD): DenseVector = v match {
+    case v: DVD => new DenseVector(v.toArray)
   }
 
   def main(args: Array[String]): Unit = {
@@ -34,23 +51,22 @@ object FitSpark {
     println(df3 show 5)
     println(df3.head.getAs[DenseVector](1))
     val p = 8
-    def ll(beta: BDV[Double]): Double = 
+    def ll(beta: DVD): Double = 
       df3.map{row =>
         val y = row.getAs[Double](0)
-        val x = BDV.vertcat(BDV[Double](1.0),toBDV(row.getAs[DenseVector](1)))
+        val x = BDV.vertcat(BDV(1.0),toBDV(row.getAs[DenseVector](1)))
         -math.log(1.0 + math.exp(-1.0*(2.0*y-1.0)*(x.dot(beta))))}.reduce(_+_)
-    def lprior(beta: BDV[Double]): Double =
+    def lprior(beta: DVD): Double =
       Gaussian(0,10).logPdf(beta(0)) +
         sum(beta(1 until p).map(Gaussian(0,1).logPdf(_)))
-    def lpost(beta: BDV[Double]): Double =
+    def lpost(beta: DVD): Double =
       ll(beta) + lprior(beta)
-    val pre = BDV[Double](10.0,1.0,1.0,1.0,1.0,1.0,5.0,1.0)
+    val pre = BDV(10.0,1.0,1.0,1.0,1.0,1.0,5.0,1.0)
     def rprop(beta: DoubleState): DoubleState = beta + pre *:* (BDV(Gaussian(0.0,0.02).sample(p).toArray))
     def dprop(n: DoubleState, o: DoubleState): Double = 1.0
-    val init = BDV[Double](-9.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0)
-
-    val s = Mcmc.mhStream(init, lpost, rprop, dprop,
-      (p: DoubleState) => 1.0, verb = false)
+    val init = BDV(-9.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0)
+    val kern = mhKernel(lpost, rprop)
+    val s = Stream.iterate((init, -Inf))(kern) map (_._1)
     val out = s.drop(150).thin(50).take(10000)
     println("Starting RW MH run now. Be patient...")
     out.zipWithIndex.foreach(println)
