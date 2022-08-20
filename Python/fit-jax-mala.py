9ed19e8b6db1b8b1d55639a3ea2c21128e733886
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2022-08-20 15:34:21 +0100

    parquet fix

--- Python/fit-jax-mala.py
+++ Python/fit-jax-mala.py
@@ -128,7 +128,7 @@ pre = jnp.array([100.,1.,1.,1.,1.,1.,25.,1.]).astype(jnp.float32)
 out = mcmc(beta, malaKernel(lpost, glp, dt=1e-6, pre=pre), thin=2000)
 
 print(out)
-odf = pd.DataFrame(out, columns=["b0","b1","b2","b3","b4","b5","b6","b7"])
+odf = pd.DataFrame(np.asarray(out), columns=["b0","b1","b2","b3","b4","b5","b6","b7"])
 odf.to_parquet("fit-jax-mala.parquet")
 print("Posterior summaries:")
 summ = scipy.stats.describe(out)
