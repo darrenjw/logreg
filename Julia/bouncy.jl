7d7404f2057bd1d673f39cd2d841be98e821ca09
mschauer <moritzschauer@web.de>
2022-08-26 12:36:54 +0200

    tweaks

--- Julia/bouncy.jl
+++ Julia/bouncy.jl
@@ -7,7 +7,8 @@ using LinearAlgebra
 # Load and process the data
 df = DataFrame(load(joinpath(@__DIR__, "..", "pima.parquet")))
 y = map((yi) -> yi == "Yes" ? 1.0 : 0.0, df.type)
-X = hcat(ones(200, 1), Matrix(df[:,1:7]))
+d = 8 # number of parameters 
+X = hcat(ones(length(y), 1), Matrix(df[:,1:d-1]))
 
 # Prior
 prior = MvNormal(Diagonal([10.0; ones(7)].^2))
@@ -17,15 +18,15 @@ ll(β, X, y) = sum(-log.(1.0 .+ exp.(-(2*y .- 1.0).*(X * β))))
 ϕ(β, prior, X, y) = -(logpdf(prior, β) + ll(β, X, y)) # potential
 
 # Actually, we need a gradient... 
-function ∇ϕ!(out, t, β, _, _, prior, X, y) # gradient of ϕ
+function ∇ϕ!(out, t, β, v, _, prior, X, y) # gradient of ϕ
     @. out = -β/prior.Σ.diag 
     out .+= transpose(X) * (y .- (1 ./ (1 .+ exp.(-X * β))))
     out
 end
 
 # ... and directional derivatives we get with ForwardDiff
-function dϕ(t, x, v, _, prior, X, y) # two directional derivatives of ϕ
-    u = ForwardDiff.derivative(t -> ϕ(x + t*v, prior, X, y), ForwardDiff.Dual{:dϕ}(0.0, 1.0))
+function dϕ(t, β, v, _, prior, X, y) # two directional derivatives of ϕ
+    u = ForwardDiff.derivative(t -> ϕ(β + t*v, prior, X, y), ForwardDiff.Dual{:dϕ}(0.0, 1.0))
     u.value, u.partials[]
 end
 
@@ -34,14 +35,13 @@ M = ZigZagBoomerang.PDMats.PDiagMat([3.5, 0.0038, 4.4e-5, 0.00036, 0.00052, 0.00
 
 # Going to use BouncyParticle sampler 
 Z = BouncyParticle(missing, missing, # ignored
-    2.0, # momentum refreshment rate 
-    0.9, # momentum correlation / only gradually change momentum in refreshment/momentum update
+    0.5, # momentum refreshment rate 
+    0.7, # momentum correlation / only gradually change momentum in refreshment/momentum update
     M, # metric
     missing
 ) 
 
 # Initialize sampler
-d = 8 # number of parameters 
 x0 = zeros(d) # starting point sampler
 θ0 = randn(d) # starting direction sampler
 T = 4000. # end time (similar to number of samples in MCMC)
@@ -69,7 +69,7 @@ plot(ts, out, layout=(4, 2), label=labels)
 savefig("tracebouncy.pdf")
 histogram(out, layout=(4, 2), label=labels)
 savefig("histbouncy.pdf")
-plot(1:200, autocor(out, 1:200), label=labels, layout = (4, 2))
+scatter(1:100, autocor(out, 1:100), label=labels, layout = (4, 2))
 savefig("acfbouncy.pdf")
 plot(getindex.(xs,1), getindex.(xs,2), xlabel= labels[1], ylabel=labels[2], label="trace")
 savefig("pairbouncy.pdf")
